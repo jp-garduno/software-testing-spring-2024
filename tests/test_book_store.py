3aac9d69b499d0434ba8e6663c469e41a18e2524
Garduno Calderon, Juan P <juan.p.garduno.calderon@intel.com>
2024-11-06 16:04:46 -0600

    chore: update selenium tests
    Update selenium tests to work with newer Chrome versions.

--- tests/test_book_store.py
+++ tests/test_book_store.py
@@ -4,6 +4,7 @@
 Book store unit testing examples.
 """
 import unittest
+from unittest.mock import patch
 
 from src.book_store import Book, BookStore
 
@@ -13,19 +14,36 @@ class TestBook(unittest.TestCase):
     Book unittest class.
     """
 
+    def setUp(self):
+        """
+        Creates the book properties
+        """
+        self.title = "title"
+        self.author = "author"
+        self.price = 9.99
+        self.quantity = 5
+
     def test_book_init(self):
         """
         Checks the book properties.
         """
-        title = "title"
-        author = "author"
-        price = 9.99
-        quantity = 5
-        book = Book(title, author, price, quantity)
-        self.assertEqual(book.title, title)
-        self.assertEqual(book.author, author)
-        self.assertEqual(book.price, price)
-        self.assertEqual(book.quantity, quantity)
+        book = Book(self.title, self.author, self.price, self.quantity)
+        self.assertEqual(book.title, self.title)
+        self.assertEqual(book.author, self.author)
+        self.assertEqual(book.price, self.price)
+        self.assertEqual(book.quantity, self.quantity)
+
+    @patch("builtins.print")
+    def test_book_display(self, mock_print):
+        """
+        Checks the book display function.
+        """
+        book = Book(self.title, self.author, self.price, self.quantity)
+        book.display()
+        self.assertTrue(mock_print.called)
+        self.assertEqual(mock_print.call_count, 4)
+        mock_print.assert_any_call(f"Title: {self.title}")
+        mock_print.assert_called_with(f"Quantity: {self.quantity}")
 
 
 class TestBookStore(unittest.TestCase):
